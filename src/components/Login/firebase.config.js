41a89faa22f12f1e24b8465f4f55226f363468fa
Ahmmad Hossain <ahmmad.bd@gmail.com>
2020-10-06 02:23:30 +0600

    commit

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,9 +1,5 @@
-// Firebase App (the core Firebase SDK) is always required and must be listed first
 import * as firebase from "firebase/app";
-
-// Add the Firebase products that you want to use
 import "firebase/auth";
-// import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCLMIi0PLcP7l4JZ6mQdchCLEN5DOkcHhY",
@@ -14,7 +10,6 @@ const firebaseConfig = {
   messagingSenderId: "656862916181",
   appId: "1:656862916181:web:7af8d493d24aa8c0172edc",
 };
-
 firebase.initializeApp(firebaseConfig);
 
 export default firebase;
